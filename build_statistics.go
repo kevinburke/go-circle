739f2448ec2459c119e7095b6e74ec3adba94fa3
Kevin Burke <kev@inburke.com>
2018-05-03 15:23:07 -0700

    Switch up time printing
    Right align timestamps, and try a little bit to align the duration
    columns so they're easier to read.

--- build_statistics.go
+++ build_statistics.go
@@ -1,18 +1,14 @@
 package circle
 
 import (
-	"bytes"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
-
-func roundDuration(d CircleDuration, unit time.Duration) time.Duration {
-	return ((time.Duration(d) + unit/2) / unit) * unit
-}
 
 const stepWidth = 45
 
@@ -22,10 +18,37 @@ func isatty() bool {
 	return terminal.IsTerminal(int(os.Stdout.Fd()))
 }
 
+var forceNonZeroTestVal = time.Duration(0)
+
+// timeScaler returns a format string for the given Duration where all of the
+// decimals will line up in the same column (fourth from the end).
+func timeScaler(d time.Duration) string {
+	if d == 0 && forceNonZeroTestVal != 0 {
+		d = forceNonZeroTestVal
+	}
+	switch {
+	case d == 0:
+		return "0.0ms"
+	case d >= time.Minute:
+		mins := d / time.Minute
+		d = d - mins*time.Minute
+		s := strconv.FormatFloat(float64(d.Nanoseconds())/1e9, 'f', 0, 64)
+		return strconv.Itoa(int(mins)) + "m" + fmt.Sprintf("%02s", s) + "s"
+	case d >= time.Second:
+		return strconv.FormatFloat(float64(d.Nanoseconds())/1e9, 'f', 1, 64) + "s"
+	case d >= 50*time.Microsecond:
+		return strconv.FormatFloat(float64(d.Nanoseconds())/1e9*1000, 'f', 0, 64) + "ms"
+	case d >= time.Microsecond:
+		return strconv.FormatFloat(float64(d.Nanoseconds())/1e9*1000*1000, 'f', 0, 64) + "µs"
+	default:
+		return strconv.FormatFloat(float64(d.Nanoseconds()), 'f', 0, 64) + "ns"
+	}
+}
+
 // Statistics prints out statistics for the given build. If stdout is a TTY,
 // failed builds will be surrounded by red ANSI escape sequences.
 func (cb *CircleBuild) Statistics() string {
-	var b bytes.Buffer
+	var b strings.Builder
 	b.WriteString(fmt.Sprintf(stepPadding, "Step"))
 	l := stepWidth
 	for i := uint8(0); i < cb.Parallel; i++ {
@@ -44,18 +67,16 @@ func (cb *CircleBuild) Statistics() string {
 		for _, action := range step.Actions {
 			var dur time.Duration
 			if time.Duration(action.Runtime) > time.Minute {
-				dur = roundDuration(action.Runtime, time.Second)
+				dur = time.Duration(action.Runtime).Round(time.Second)
 			} else {
-				dur = roundDuration(action.Runtime, time.Millisecond*10)
+				dur = time.Duration(action.Runtime).Round(time.Millisecond * 10)
 			}
-			var durString string
 			if action.Failed() && isatty() {
 				// color the output red
-				durString = fmt.Sprintf("\033[38;05;160m%-8s\033[0m", dur.String())
+				fmt.Fprintf(&b, "\033[38;05;160m%8s\033[0m", timeScaler(dur))
 			} else {
-				durString = fmt.Sprintf("%-8s", dur.String())
+				fmt.Fprintf(&b, "%8s", timeScaler(dur))
 			}
-			b.WriteString(durString)
 		}
 		b.WriteString("\n")
 	}
